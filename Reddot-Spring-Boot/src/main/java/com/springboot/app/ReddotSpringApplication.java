8bba23a4287ab09089c2b84233a6da735fdc0205
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-01-10 16:49:41 +0800

    refactor: rename application and test classes for consistency; update property keys for OAuth2 configuration

--- Reddot-Spring-Boot/src/main/java/com/springboot/app/ReddotSpringApplication.java
+++ Reddot-Spring-Boot/src/main/java/com/springboot/app/ReddotSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbapiApplication {
+public class ReddotSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SbapiApplication.class, args);
+        SpringApplication.run(ReddotSpringApplication.class, args);
     }
 
 }
